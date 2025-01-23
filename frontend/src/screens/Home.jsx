7dd7e3ae9c0772d38208f74fae6bd233b60a05d6
Jaimin <jaiminkaneriya@gmail.com>
2025-01-23 21:02:39 +0530

    add user context

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../context/user.context';
 
 const Home = () => {
+
+    const { user } = useContext(UserContext) 
+
     return (
-        <div>
-            Homee
-        </div>
+        <div>{JSON.stringify(user)}</div>
     )
 }
 
